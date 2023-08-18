f95439cff6ef4549910807eba67135d1ba054135
ngr-np <ngr112001@gmail.com>
2023-08-18 17:54:32 +0545

    added  tags and categorys id as reference in blogs collection

--- api/model/blogModel.js
+++ api/model/blogModel.js
@@ -1,25 +1,42 @@
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    require: [true, "title is required!!"],
-    minLength: 5,
-    maxLength: 30,
-  },
-  description: {
-    type: String,
-    require: [true, "description is required!!"],
-    maxLength: 250,
-  },
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: [true, "title is required!!"],
+      minLength: 5,
+      maxLength: 30,
+    },
+    description: {
+      type: String,
+      require: [true, "description is required!!"],
+      maxLength: 250,
+    },
 
-  author: {
-    type: String,
-    require: [true, "author name is required!!"],
+    author: {
+      type: String,
+      require: [true, "author name is required!!"],
+    },
+    image: {
+      type: String,
+    },
+    tags: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "tags",
+      },
+    ],
+    categorys: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "categorys",
+      },
+    ],
   },
-  image: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Blogs", blogSchema);
